72c295d0c610741315a7812596b6283baa5c2f00
github username <github email address>
2019-07-01 16:10:06 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -11,12 +11,16 @@ ALL = []
     end 
     # call this on Monica
     def pass_student(student_obj, test_nam)
+       if test_nam == nil 
+            BoatingTest.new(student_obj, test_nam, "Pass", Instructor.new("Chica"))
+       else   
         BoatingTest.all.each do |test_obj|
             if test_obj.student_inst == student_obj && test_obj.test_name == test_nam
                 test_obj.test_status = "Pass"
             end 
             
         end 
+    end  
 
 
     end 
